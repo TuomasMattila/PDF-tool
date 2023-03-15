78c05854b9535d10f24e7758af17eb2e04df7d04
TuomasMattila <tuomas.mattila@windowslive.com>
2023-03-15 10:23:39 +0200

    Added changelog to readme, implemented main function structure

--- PDF_tool.py
+++ PDF_tool.py
@@ -13,64 +13,69 @@ The new pdf with the bookmarks replaces the old pdf.
 import pypdf
 import sys
 
-print("\n--- PDF bookmark generator ---")
 
-while True:
-    try:
-        filename = sys.argv[1]
-        reader = pypdf.PdfReader(filename)
-    except:
-        invalid_file = True
-        while invalid_file:
-            filename = input("\nGive the name of the PDF file (Q to quit): ")
-            if filename.lower() == "q":
-                quit()
-            else:
-                try:
-                    reader = pypdf.PdfReader(filename)
-                    invalid_file = False
-                except:
+def main():
+
+    print("\n--- PDF tool ---")
+
+    while True:
+        try:
+            filename = sys.argv[1]
+            reader = pypdf.PdfReader(filename)
+        except:
+            invalid_file = True
+            while invalid_file:
+                filename = input("\nGive the name of the PDF file (Q to quit): ")
+                if filename.lower() == "q":
+                    quit()
+                else:
                     try:
-                        reader = pypdf.PdfReader(filename + ".pdf")
+                        reader = pypdf.PdfReader(filename)
                         invalid_file = False
                     except:
-                        print("Invalid file, try again.")
+                        try:
+                            reader = pypdf.PdfReader(filename + ".pdf")
+                            invalid_file = False
+                        except:
+                            print("Invalid file, try again.")
 
-    print(f"\nReading {filename} with {len(reader.pages)} pages...")
-    
-    if reader.outline:
-        answer = input("This PDF already contains bookmarks,\nAre you sure you want to replace them? (Y/N): ")
-        if answer.lower() != 'y':
-            continue
+        print(f"\nReading {filename} with {len(reader.pages)} pages...")
+        
+        if reader.outline:
+            answer = input("This PDF already contains bookmarks,\nAre you sure you want to replace them? (Y/N): ")
+            if answer.lower() != 'y':
+                continue
 
+        writer = pypdf.PdfWriter()
+        num_bookmarks = 0
 
-    writer = pypdf.PdfWriter()
-    num_bookmarks = 0
+        for page_num, page in enumerate(reader.pages):
+            writer.add_page(page)
+            text = page.extract_text()
+            # If there is no text on a page, we will just skip it
+            try:
+                heading = text[:text.index("\n")]
+            except:
+                continue
+            writer.add_outline_item(heading, page_num)
+            num_bookmarks += 1
 
-    for page_num, page in enumerate(reader.pages):
-        writer.add_page(page)
-        text = page.extract_text()
-        # If there is no text on a page, we will just skip it
+        writer.add_metadata(reader.metadata)
+
+        if filename.endswith(".pdf"):
+            # new_filename = filename
+            new_filename = filename[:filename.index(".pdf")] + "_new" + ".pdf"
+        else:
+            # new_filename = filename + ".pdf"
+            new_filename = filename + "_new" + ".pdf"
+
         try:
-            heading = text[:text.index("\n")]
+            with open(f"{new_filename}", "wb") as file:
+                writer.write(file)
+            print(f"Success! Added {num_bookmarks} bookmarks to {new_filename}.")
         except:
-            continue
-        writer.add_outline_item(heading, page_num)
-        num_bookmarks += 1
+            print(f"Something went wrong when attempting to save the file {new_filename}")
+            print("Make sure you do not have a file with that name open.\n")
 
-    writer.add_metadata(reader.metadata)
-
-    if filename.endswith(".pdf"):
-        # new_filename = filename
-        new_filename = filename[:filename.index(".pdf")] + "_new" + ".pdf"
-    else:
-        # new_filename = filename + ".pdf"
-        new_filename = filename + "_new" + ".pdf"
-
-    try:
-        with open(f"{new_filename}", "wb") as file:
-            writer.write(file)
-        print(f"Success! Added {num_bookmarks} bookmarks to {new_filename}.")
-    except:
-        print(f"Something went wrong when attempting to save the file {new_filename}")
-        print("Make sure you do not have a file with that name open.\n")
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -22,9 +22,13 @@ py PDF_tool.py filename.pdf
 
 ## Upcoming features
 - Create bookmarks for multiple PDF files at a time
-- Ask whether user wants to replace bookmarks if the PDF already has them
 - Combining PDF files
 - Make the PDF's file name also the title for the PDF
   - Title is a part of the metadata
   - Title is often displayed in PDF viewing software instead of file name
 - GUI, maybe
+
+## Changelog
+15.3.2023: Adding bookmarks can be cancelled if the PDF already has them
+15.3.2023: Can add bookmarks to a PDF file
+15.3.2023: Initialized repository

9c50a6da6a69cc7bfa83226ad0422f174d85b988
TuomasMattila <tuomas.mattila@windowslive.com>
2023-03-25 16:29:19 +0200

    Bookmarks can be viewed in app (WIP), but the implementation has a lot of bugs still

--- pdf_tool/app.py
+++ pdf_tool/app.py
@@ -46,6 +46,22 @@ class PDFList(ctk.CTkScrollableFrame):
                 cb.deselect()
 
 
+class BookmarkList(ctk.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.columnconfigure(0, weight=1)
+        self.bookmarks_list = []
+
+    def update_list(self, bookmarks: list):
+        for bookmark in self.bookmarks_list:
+            bookmark.destroy()
+        self.bookmarks_list = []
+        for bookmark in bookmarks:
+            lbl_bookmark = ctk.CTkLabel(self, text=bookmark)
+            lbl_bookmark.grid(row=len(self.bookmarks_list), column=0, sticky='nw')
+            self.bookmarks_list.append(lbl_bookmark)
+
+
 class App(ctk.CTk):
     """
     Class for the GUI.
@@ -61,12 +77,11 @@ class App(ctk.CTk):
         self.columnconfigure((0, 1), weight=1)
         self.rowconfigure(0, weight=1)
 
+
         self.frm_main = ctk.CTkFrame(self)
         self.frm_main.columnconfigure(0, weight=1)
-
         self.lbl_choose_pdf = ctk.CTkLabel(self.frm_main, text='PDF files')
         self.lbl_choose_pdf.grid(row=0, column=0, pady=(PADDING, 0), sticky='nsew')
-
 
         # PDF list section
         self.frm_pdf_list_section = ctk.CTkFrame(self.frm_main)
@@ -86,55 +101,50 @@ class App(ctk.CTk):
 
         self.pdf_list = PDFList(self.frm_pdf_list_section, fg_color='transparent')
         self.pdf_list.grid(row=2, column=0, columnspan=3, pady=(0, PADDING/2), padx=(PADDING/2-1, PADDING/2+1), sticky='nsew')
+        self.bind('<Button-1>', self.on_pdf_selected)
 
         self.checkbox_select_all.configure(command=lambda: self.pdf_list.toggle_all(self.checkbox_select_all.get()))
         self.btn_remove.configure(command=self.pdf_list.remove_selected)
 
         self.frm_pdf_list_section.grid(row=1, column=0, sticky='nsew', padx=PADDING, pady=PADDING)
 
-
         # Buttons below PDF list
         self.frm_buttons = ctk.CTkFrame(self.frm_main, fg_color='transparent')
-
         self.btn_generate_bookmarks = ctk.CTkButton(self.frm_buttons,
                                                  text='Generate bookmarks',
                                                  width=20,
                                                  command=self.generate_bookmarks)
         self.btn_generate_bookmarks.pack(pady=(PADDING, 0))
-
         self.btn_combine_pdfs = ctk.CTkButton(self.frm_buttons,
                                            text='Combine PDF files',
                                            width=20,
                                            command=self.combine_pdfs)
         self.btn_combine_pdfs.pack(pady=(PADDING, 0))
-
         self.btn_save_changes = ctk.CTkButton(self.frm_buttons,
                                            text='Save changes',
                                            width=20,
                                            command=self.save_changes)
         self.btn_save_changes.pack(pady=(PADDING, 0))
-
         self.btn_quit = ctk.CTkButton(self.frm_buttons,
                                    text='Quit',
                                    width=20,
                                    command=self.quit)
         self.btn_quit.pack(pady=(PADDING, 0))
-
         self.frm_buttons.grid(row=2, column=0, sticky='nsew')
-        
+
         self.frm_main.grid(row=0, column=0, sticky='nsew', ipadx=PADDING, ipady=PADDING, padx=PADDING, pady=(PADDING, 0))
 
 
         # Bookmark list
         self.frm_bookmark_list = ctk.CTkFrame(self)
-
         self.lbl_bookmarks = ctk.CTkLabel(self.frm_bookmark_list, text='Bookmarks')
         self.lbl_bookmarks.pack(pady=PADDING)
-
-
+        self.bookmark_list = BookmarkList(self.frm_bookmark_list, fg_color='transparent')
+        self.bookmark_list.pack(fill='both', expand=True, padx=PADDING/2, pady=(0, PADDING))
         self.frm_bookmark_list.grid(row=0, column=1, sticky='nsew', padx=(0, PADDING), pady=(PADDING, 0))
 
 
+        # Statusbar
         self.frm_statusbar = ctk.CTkFrame(self)
         self.lbl_status = ctk.CTkLabel(self.frm_statusbar, text="Choose PDF files to begin")
         self.lbl_status.pack()
@@ -156,7 +166,6 @@ class App(ctk.CTk):
         for filename in selected:
             self.data.remove_pdf(filename)
         self.update_status("Removed selected files")
-
     
     def generate_bookmarks(self):
         self.update_status("Generating bookmarks...")
@@ -180,6 +189,17 @@ class App(ctk.CTk):
 
     def update_status(self, message: str):
         self.lbl_status.configure(text=message)
+
+    def on_pdf_selected(self, event):
+        if len(self.pdf_list.get_selected()) == 1:
+            selection = self.pdf_list.get_selected()[0]
+            try:
+                bookmarks = self.data.get_bookmarks(selection)[0] # TODO: bookmarks is empty for some reason after clicking Save changes
+            except:
+                print(self.data.get_bookmarks(selection))
+            self.bookmark_list.update_list(bookmarks)
+        else:
+            self.bookmark_list.update_list([])
 
     def quit(self):
         self.destroy()
